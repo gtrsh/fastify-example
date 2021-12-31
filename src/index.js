103368c3dfa7649b3f80132eee732c44d5a56d05
Gregory Tereshko <gtrsh@users.noreply.github.com>
2021-12-31 21:08:19 +0300

    add .env lib and config

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-import { spawn } from 'child_process'
+import { getMetaData } from './metadata.js'
 
-const getMetaData = (url) => spawn('yt-dlp', ['--dump-single-json', url])
-
-getCoubDataObject('https://coub.com/view/2nmqv5')
-  .stdout
-  .on('data', (data) => console.log(JSON.parse(data.toString())))
-
-getMetaData('https://coub.com/view/2nmqasfd33333333333v5')
-  .stderr
-  .on('data', (data) => console.log(data.toString()))
-
+const data = await getMetaData('https://coub.com/view/2nmqv5')
+console.log(data.toString())
+console.log(process.env.PG_DB)
