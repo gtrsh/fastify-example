7cf34f2ca4021835695cbfb36cdb8ec30af1bbee
Gregory Tereshko <gtrsh@users.noreply.github.com>
2022-01-21 15:57:52 +0300

    minio example

--- src/index.js
+++ src/index.js
@@ -3,3 +3,13 @@ import { getMetaData } from './metadata.js'
 const data = await getMetaData('https://coub.com/view/2nmqv5')
 console.log(data.toString())
 console.log(process.env.PG_DB)
+
+import { client } from './dal'
+
+client.listBuckets(function(err, buckets) {
+  if (err) return console.log(err)
+  console.log('buckets :', buckets)
+})
+
+const stream = client.listObjectsV2('coub-bucket','', true,'')
+stream.on('data', function(obj) { console.log(obj) } )
