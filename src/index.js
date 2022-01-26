4cac8abb3d95cd41ed413a11ffa31c1d230f7da0
Gregory Tereshko <gtrsh@users.noreply.github.com>
2022-01-26 14:05:33 +0300

    example of uploading coub to minio s3

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
 import { request } from 'undici'
-import { createWriteStream } from 'node:fs'
 import { getMetaData } from './metadata'
+import { getFileName } from './utils'
 
-const data = await getMetaData('https://coub.com/view/2nmqv5')
+const data = await getMetaData('https://coub.com/view/1v0g2g')
 const { requested_formats } = JSON.parse(data.toString())
 console.log(requested_formats)
 const { url: videoUrl, filesize: videoSize } = requested_formats.find((i) => i.video_ext === 'mp4')
 const { url: audioUrl, filesize: audioSize } = requested_formats.find((i) => i.audio_ext === 'mp3')
 
-console.log(videoUrl)
-console.log(videoSize)
-console.log('==========')
-console.log(audioUrl)
-console.log(audioSize)
-
-const { statusCode, body: bodyStream } = await request(videoUrl)
-bodyStream.pipe(createWriteStream('test.mp4'))
+const { statusCodeVid, body: bodyStreamVid } = await request(videoUrl)
+const { statusCodeAud, body: bodyStreamAud } = await request(audioUrl)
 
 import { client } from './dal'
+
+client.putObject('coub-bucket', getFileName(videoUrl), bodyStreamVid, videoSize, (err, objInfo) => {
+  if(err) {
+    return console.log(err) // err should be null
+  }
+  console.log("upload complete")
+  console.log(objInfo)
+})
+
+client.putObject('coub-bucket', getFileName(audioUrl), bodyStreamAud, audioSize, (err, objInfo) => {
+  if(err) {
+    return console.log(err) // err should be null
+  }
+  console.log("upload complete")
+  console.log(objInfo)
+})
